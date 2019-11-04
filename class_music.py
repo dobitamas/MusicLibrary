3a968e1cc4d2982b5cc4cabcc467a268525c150e
dobitamas <totika10@gmail.com>
2019-11-04 14:42:21 +0100

    Minden file

---
+++ class_music.py
@@ -0,0 +1,7 @@
+class music:
+   def __init__(self, artist_name, album_name, release_year, genre, lenght):
+      self.artist_name = artist_name
+      self.album_name = album_name
+      self.release_year = release_year
+      self.genre = genre
+      self.lenght = lenght
--- rw.py
+++ rw.py
@@ -1,18 +1,29 @@
 from class_music import music
 album = []
-def open_to_list(filename,music1):
+music1 = []
+def open_to_list(music1):
     try:
-        data = open("/home/mate/Asztal/python/music.library/{}".format(filename), "r")
+        data = open("Music.txt", "r")
         for line in data:
             music1.append(line.split(","))
-        return(music1)
         data.close()
+        return(music1)
     except FileNotFoundError:
         print("File not found!")
     
 def put_in_class(music1):
     for i in range(len(music1)):
-        album.append(music(music1[i][0],music1[i][1],music1[i][2],music1[i][3],music1[i][4][:-1])
+        album.append(music(music1[i][0],music1[i][1],music1[i][2],music1[i][3],music1[i][4][:-1]))
     return(album)
 
 
+def save_to_list(album,filename):
+    writer = open(filename, "w")
+    for sor in range(len(music1)):
+        for elem in sor:
+            writer.writelines(album[sor][elem])
+    
+
+open_to_list(music1)
+put_in_class(music1)
+save_to_list(album,"Music1.txt")
