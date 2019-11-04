763cf94191b03531c7cf452a0091dc80bef7692f
dobitamas <totika10@gmail.com>
2019-11-04 13:14:43 +0100

    Up-To Date

--- rw.py
+++ rw.py
@@ -1,14 +1,18 @@
-
-music = []
-def open_to_list(filename,music):
+from class_music import music
+album = []
+def open_to_list(filename,music1):
     try:
         data = open("/home/mate/Asztal/python/music.library/{}".format(filename), "r")
         for line in data:
-            music.append(line.split())
-        return(music)
+            music1.append(line.split(","))
+        return(music1)
         data.close()
     except FileNotFoundError:
         print("File not found!")
     
-open_to_list("text_albums_data.txt",music)
-print(music)
+def put_in_class(music1):
+    for i in range(len(music1)):
+        album.append(music(music1[i][0],music1[i][1],music1[i][2],music1[i][3],music1[i][4][:-1])
+    return(album)
+
+
